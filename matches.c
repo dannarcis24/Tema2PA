c6acbca94066e0c00b4d0e7f94426e298cbc7bdb
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-28 01:29:09 +0300

    rezolvare prima cerinta

--- matches.c
+++ matches.c
@@ -40,7 +40,7 @@ char *readString(FILE *f) //citirea sirurilor se face parcurgand carcter cu cara
     return sir;
 }
 
-TeamList* dataInput(char *fisier)
+List* dataInput(char *fisier)
 {
     FILE *f = fopen(fisier, "rt");
     if(!f)
@@ -49,7 +49,7 @@ TeamList* dataInput(char *fisier)
         exit(1);
     }
 
-    TeamList *lista_echipe = NULL;
+    List *lista_echipe = NULL;
     for(register int i = 0; i < 32 && !feof(f); i++)
     {
         Team *echipa = createTeam();
@@ -59,16 +59,53 @@ TeamList* dataInput(char *fisier)
         fseek(f, 1L, SEEK_CUR);
         echipa->nume_echipa = readString(f);
 
-        addTeam(&lista_echipe, echipa);
+        addElem(&lista_echipe, echipa);
     }
     fclose(f);
 
     return lista_echipe;
 }
 
-int** tournamentMatrix(TeamList *lista_echipe)
+List* whoWins(List *echipa1, List *echipa2)
 {
-    int **matrice_turneu = (int**)malloc(sizeof(int*) * 32);
+    if(((Team*)echipa1->val)->punctaj > ((Team*)echipa2->val)->punctaj)
+        return echipa1;
+    else
+        if(((Team*)echipa1->val)->punctaj < ((Team*)echipa2->val)->punctaj)
+            return echipa2;
+        else
+            return ((strcmp(((Team*)echipa1->val)->nume_echipa, ((Team*)echipa2->val)->nume_echipa) > 0 ? echipa1 : echipa2));    
+}
+
+void match(Queue *castigatori, Queue *pierzatori, int runda, int *matrice)
+{
+    for(register int i = 0; i < runda; i++)
+    {
+        List *echipa1 = deQueue(castigatori);
+        List *echipa2 = deQueue(castigatori);
+
+        List *win = whoWins(echipa1, echipa2);
+
+        if(win == echipa1)
+        {
+            matrice[((Team*)echipa2->val)->pozitie * 32 + ((Team*)echipa1->val)->pozitie] = 1;
+
+            enQueue(castigatori, echipa1);
+            enQueue(pierzatori, echipa2);
+        }
+        else
+        {
+            matrice[((Team*)echipa1->val)->pozitie * 32 + ((Team*)echipa2->val)->pozitie] = 1;
+
+            enQueue(castigatori, echipa2);
+            enQueue(pierzatori, echipa1);
+        }
+   }
+}
+
+int* tournamentMatrix(List *lista_echipe)
+{
+    int *matrice_turneu = (int*)malloc(sizeof(int) * 32 * 32);
     if(!matrice_turneu)
     {
         printf("variabilei matrice turneu nu i s-a putut aloca memorie");
@@ -77,7 +114,44 @@ int** tournamentMatrix(TeamList *lista_echipe)
 
     for(register int i = 0; i < 32; i++)
         for(register int j = 0; j <= i; j++)
-            matrice_turneu[i][j] = 0;
+            matrice_turneu[i * 32 + j] = matrice_turneu[j * 32 + i] = 0;
 
-    
+    Queue *castigatori = createQueue();
+    for(register List *p = lista_echipe; p; p = p->next) 
+        enQueue(castigatori, p);
+
+    int numar_runde, numar_echipe;
+    numar_runde = numar_echipe = 16;
+    numar_runde++;
+    while(numar_runde)
+    {
+        Queue *pierzatori = createQueue();
+
+        match(castigatori, pierzatori, numar_echipe, matrice_turneu);
+        numar_runde--;
+        numar_echipe /= 2;
+
+        delQueue(&pierzatori);
+    }
+    delQueue(&castigatori);
+
+    return matrice_turneu;
+}
+
+void writeMatrix(int *matrice, char *fisier)
+{
+    FILE *f = fopen(fisier, "wt");
+    if(!f)
+    {
+        printf("nu s-a putut deschide fisierul %s\n", fisier);
+        exit(1);
+    }
+
+    for(register int i = 0; i < 32; i++)
+    {
+        for(register int j = 0; j < 32; j++)
+            fprintf(f, "%d ", matrice[i * 32 + j]);
+        fprintf(f, "\n");
+    }
+    fclose(f);
 }
