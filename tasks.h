f17f22796ea40489321148321e58f5ebc484b15a
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-28 21:12:57 +0300

    comentarii

--- tasks.h
+++ tasks.h
@@ -6,12 +6,12 @@ char* readString(FILE*);
 // APELURI PENTRU FUNCTII GESTIONAREA ECHIPELOR
 //          (citire, verificare, turnir, afisare, calculare punctaj)
 List* dataInput(char*);
-List* whoWIns(List*, List*);
+List* whoWIns(List*, List*); // calculeaza echipa castigatoare
 void match(Queue*, Queue*, int, int*);
 int* tournamentMatrix(List*, Queue**);
 void writeMatrix(int*, char*);
 void pointsTeam(List*, int*);
 
 // APELURI PENTRU FUNCTII DE DIVERSE CALCULE
-int wins(int*, int);
+int wins(int*, int); // calculeaza numarul de victorii al unei echipe, date prin pozitia sa in matrice
 float power(float, int);
