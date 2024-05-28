43a98dda1512eee7e42802ab0a4a2a07fafb0c38
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-28 21:14:42 +0300

    comentarii

--- score.c
+++ score.c
@@ -1,9 +1,9 @@
 #include "tasks.h"
 
-int wins(int *matrice, int poz)
+int wins(int *matrice, int poz) // se calculeaza numarul de victorii echipei de pe pozitia poz
 {
     int suma = 0;
-    for(register int i = 0; i < 32; i++)
+    for(register int i = 0; i < 32; i++) // matrice[i * 32 + j] = matrice[i][j]
         suma += matrice[i * 32 + poz];
     return suma;
 }
@@ -16,7 +16,7 @@ float power(float numar, int putere)
     return p;
 }
 
-void pointsTeam(List *lista_echipe, int *matrice_turneu)
+void pointsTeam(List *lista_echipe, int *matrice_turneu) // se modifica punctajele conform formulei
 {
     for(register List *p = lista_echipe; p; p = p->next)
     {
