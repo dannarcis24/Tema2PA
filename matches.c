ca43a4685ac929139e6afc0069c8cdae573fa6b6
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-28 21:14:06 +0300

    comentarii

--- matches.c
+++ matches.c
@@ -1,6 +1,6 @@
 #include "tasks.h"
 
-char *readString(FILE *f) //citirea sirurilor se face parcurgand carcter cu caracter fisierul
+char *readString(FILE *f) // citirea sirurilor se face parcurgand carcter cu caracter fisierul
 {
     char c, *sir = (char *)malloc(sizeof(char));
     if (!sir)
@@ -11,7 +11,7 @@ char *readString(FILE *f) //citirea sirurilor se face parcurgand carcter cu cara
     int k = 0;
     
     fscanf(f, "%c", &c);
-    while (c != '\n')
+    while (c != '\n') // citirea se faca pana la '\n', astfel se retin spatiile din sir
     {   
         sir[k++] = c;
         fscanf(f, "%c", &c);
@@ -26,7 +26,7 @@ char *readString(FILE *f) //citirea sirurilor se face parcurgand carcter cu cara
             break;
     }
     sir[k] = '\0';
-    if(sir[strlen(sir)-1] == '\r')
+    if(sir[strlen(sir)-1] == '\r') // se verifica si sterge caracterul '\r'
     {
         sir[--k] = '\0';
         sir = (char *)realloc(sir, sizeof(char) * (k + 1));
@@ -50,13 +50,13 @@ List* dataInput(char *fisier)
     }
 
     List *lista_echipe = NULL;
-    for(register int i = 0; i < 32 && !feof(f); i++)
+    for(register int i = 0; i < 32 && !feof(f); i++) // se citesc si introduc in lista cele 32echipe
     {
         Team *echipa = createTeam();
 
         fscanf(f, "%f", &echipa->punctaj);
         echipa->pozitie = i;
-        fseek(f, 1L, SEEK_CUR);
+        fseek(f, 1L, SEEK_CUR); // se respecta formatul, in felul acesta numele echipei mu va incepe cu spatiu
         echipa->nume_echipa = readString(f);
 
         addElem(&lista_echipe, echipa);
@@ -79,14 +79,14 @@ List* whoWins(List *echipa1, List *echipa2)
 
 void match(Queue *castigatori, Queue *pierzatori, int runda, int *matrice)
 {
-    for(register int i = 0; i < runda; i++)
+    for(register int i = 0; i < runda; i++) // la fiecare pas se extrag 2echipe si se pune echipa in lista corespunzatoare
     {
         List *echipa1 = deQueue(castigatori);
         List *echipa2 = deQueue(castigatori);
 
         List *win = whoWins(echipa1, echipa2);
 
-        if(win == echipa1)
+        if(win == echipa1) // se construieste matricea de adiacenta matrice[i][j] = matrice[32*i+j] = 1, daca echipa de pe pozitia j a castigat
         {
             matrice[((Team*)echipa2->val)->pozitie * 32 + ((Team*)echipa1->val)->pozitie] = 1;
 
@@ -103,7 +103,7 @@ void match(Queue *castigatori, Queue *pierzatori, int runda, int *matrice)
    }
 }
 
-int* tournamentMatrix(List *lista_echipe)
+int* tournamentMatrix(List *lista_echipe, Queue **lista_invinsi)
 {
     int *matrice_turneu = (int*)malloc(sizeof(int) * 32 * 32);
     if(!matrice_turneu)
@@ -112,24 +112,22 @@ int* tournamentMatrix(List *lista_echipe)
         exit(1);
     }
 
-    for(register int i = 0; i < 32; i++)
+    for(register int i = 0; i < 32; i++) // matrice[i * 32 + j] = matrice[i][j]
         for(register int j = 0; j <= i; j++)
             matrice_turneu[i * 32 + j] = matrice_turneu[j * 32 + i] = 0;
 
     Queue *castigatori = createQueue();
+    *lista_invinsi = createQueue();
     for(register List *p = lista_echipe; p; p = p->next) 
         enQueue(castigatori, p);
 
     int numar_echipe = 16;
     while(numar_echipe)
     {
-        Queue *pierzatori = createQueue();
-
-        match(castigatori, pierzatori, numar_echipe, matrice_turneu);
-        numar_echipe /= 2;
-
-        delQueue(&pierzatori);
+        match(castigatori, *lista_invinsi, numar_echipe, matrice_turneu);
+        numar_echipe /= 2; // se tine cont de numarul de meciuri dintr-o runda
     }
+    enQueue(*lista_invinsi, deQueue(castigatori));
     delQueue(&castigatori);
 
     return matrice_turneu;
@@ -144,7 +142,7 @@ void writeMatrix(int *matrice, char *fisier)
         exit(1);
     }
 
-    for(register int i = 0; i < 32; i++)
+    for(register int i = 0; i < 32; i++) // matrice[i * 32 + j] = matrice[i][j]
     {
         for(register int j = 0; j < 32; j++)
             fprintf(f, "%d ", matrice[i * 32 + j]);
